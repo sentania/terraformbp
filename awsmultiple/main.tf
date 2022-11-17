87ab978a047932e34a328dce48d97353bf17eb7d
Scott R Bowe <scottb@sentania.net>
2022-11-17 07:33:21 -0600

    Update main.tf

--- awsmultiple/main.tf
+++ awsmultiple/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
     version = "2.69.0"
-    region="us-east-1"
+    region="us-east-2"
 }
 
 resource "aws_instance" "machine1" {
     ami           = "ami-04b9e92b5572fa0d1"
     instance_type = "t2.micro"
-    availability_zone = "us-east-1b"
+    availability_zone = "us-east-2b"
     tags = {
       "type" = var.myTag
     }
@@ -15,7 +15,7 @@ resource "aws_instance" "machine1" {
 resource "aws_instance" "machine2" {
     ami           = "ami-04b9e92b5572fa0d1"
     instance_type = "t2.micro"
-    availability_zone = "us-east-1b"
+    availability_zone = "us-east-2b"
     tags = {
       "type" = var.myTag
     }
